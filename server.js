1cd62cbbd4496556114692d8351deda1d9053fed
Lady Trell <antrellkent@gmail.com>
2022-06-04 18:15:25 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const routes = require('./controllers');
+const env = require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
